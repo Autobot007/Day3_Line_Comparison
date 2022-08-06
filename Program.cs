b0fbbaca8eef9fe03074c65c48a8bfc0d22ad354
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-06 15:58:46 +0530

    [SARFARAAZ]- first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day3_LineComparison
+{
+    public class LineComparison
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Line Comparison ");
+
+        }
+    }
+}
